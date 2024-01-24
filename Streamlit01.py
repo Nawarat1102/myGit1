671e10b6a8c7ccfb35c0b874e17b3da81429774c
Nawarat1102 <nawarat.srianan@gmail.com>
2024-01-24 10:02:41 +0700

    Update Streamlit01.py
    .

--- Streamlit01.py
+++ Streamlit01.py
@@ -4,3 +4,9 @@ import pandas as pd
 st.title("การทดสอบสร้างเว็บด้วยPython")
 st.image("data.jpeg")
 st.header("การนำเสนอข้อมูลกราฟด้วย Python")
+
+col1, col2, col3 = st.columns(3)
+
+with col1: 
+with col2:
+with col3:
