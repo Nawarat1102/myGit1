20fe3d01c3dbc3b4e0807b3cf38f67b4ae1eb8e5
Nawarat1102 <nawarat.srianan@gmail.com>
2024-01-24 10:30:42 +0700

    .
    .

--- Streamlit01.py
+++ Streamlit01.py
@@ -9,10 +9,10 @@ col1, col2, col3 = st.columns(3)
 
 with col1: 
     st.header("Versicolor")
-    st.image("https://en.m.wikipedia.org/wiki/File:Iris_versicolor_3.jpg")
+    st.image("./img/Vergicolor.jpg")
 with col2:
     st.header("Verginica")
-    st.image("https://en.m.wikipedia.org/wiki/File:Iris_virginica.jpg")
+    st.image("./img/Verginica.jpg")
 with col3:
     st.header("Setosa")
-    st.image("https://commons.wikimedia.org/wiki/File:Wild_iris_flower_iris_setosa.jpg")
+    st.image("./img/Setosa.jpg")
